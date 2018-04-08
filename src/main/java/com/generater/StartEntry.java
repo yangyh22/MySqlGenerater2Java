e1ad54ecc53d4045293a9c0521e266fca5e3fd78
451561292@qq.com <451561292@qq.com>
2018-04-08 09:51:47 +0800

    前端页面基础版（树形结构展示DEMO）

---
+++ src/main/java/com/generater/StartEntry.java
@@ -0,0 +1,27 @@
+package com.generater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Description 启动入口类
+ * @author yangyh
+ * @date 2018年4月8日
+ * @version V1.0.0
+ */
+@Controller
+@EnableAutoConfiguration
+public class StartEntry {
+
+	@RequestMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(StartEntry.class, args);
+	}
+}
