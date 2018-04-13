133580a2e6af17b645588e586fe4c4e968573e72
451561292@qq.com <451561292@qq.com>
2018-04-13 11:29:48 +0800

    修改controller 名称，提交下载的测试controller

--- src/main/java/com/generater/controller/TableInfoController.java
+++ src/main/java/com/generater/controller/TableInfoController.java
@@ -5,6 +5,11 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +30,7 @@ import freemarker.template.TemplateException;
  */
 @RestController
 @RequestMapping(value = "/tableInfo")
-public class tableInfoController {
+public class TableInfoController {
 
 	@Autowired
 	TableInfoService tableInfoService;
@@ -48,5 +53,24 @@ public class tableInfoController {
 		result.setError_info("success");
 		return result;
 	}
+	
+    @RequestMapping(value = "/media", method = RequestMethod.GET)  
+    public ResponseEntity<InputStreamResource> downloadFile( Long id)  
+            throws IOException {  
+        String filePath = "D:\\test\\Test.java";  
+        FileSystemResource file = new FileSystemResource(filePath);  
+        HttpHeaders headers = new HttpHeaders();  
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");  
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getFilename()));  
+        headers.add("Pragma", "no-cache");  
+        headers.add("Expires", "0");  
+  
+        return ResponseEntity  
+                .ok()  
+                .headers(headers)  
+                .contentLength(file.contentLength())  
+                .contentType(MediaType.parseMediaType("application/octet-stream"))  
+                .body(new InputStreamResource(file.getInputStream()));  
+    }  
 
 }
